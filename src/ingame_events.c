1d21287c599b85fedf36d9a6b4572faf1a3248f1
leandre Blanchard <leandre.blanchard@eptiech.eu>
2017-12-25 20:11:38 +0100

    fix

--- src/ingame_events.c
+++ src/ingame_events.c
@@ -9,10 +9,13 @@
 
 int		leave_ingame(window_t *window, sfEvent *event, void *data)
 {
-  (void) data;
+  data_pkt_t	*ptr;
+
+  (void) window;
+  ptr = (data_pkt_t *)data;
   if (event != NULL && event->type == sfEvtKeyPressed && event->key.code != sfKeyEscape)
     return (0);
   //TODO pause / quit screen
-  sfRenderWindow_close(window->window);
+  ptr->back = true;
   return (0);
 }
--- src/start_game.c
+++ src/start_game.c
@@ -18,15 +18,15 @@ static void	fill_data(data_pkt_t *data, window_t *window, sprite_t **sprites,
 }
 
 static void	ingame(window_t *window, sprite_t **sprites,
-		       const sfMusic *music, const sfSound *sound)
+		       sfMusic *music, const sfSoundBuffer *buffer)
 {
   data_pkt_t	data;
   evtptr_t	tab[2];
 
   init_tab_ingame(tab);
   fill_data(&data, window, sprites, NULL);
-  (void) music;
-  (void) sound;
+  (void) buffer;
+  sfMusic_play(music);
   while (sfRenderWindow_isOpen(window->window) &&
 	 data.back == false)
   {
@@ -34,6 +34,7 @@ static void	ingame(window_t *window, sprite_t **sprites,
     ptr_pollevent(window, tab, sizeof(tab) / sizeof(*tab), &data);
     window_refresh(window);
   }
+  sfMusic_stop(music);
 }
 
 void		start_game(void *data, const char *filename)
@@ -41,21 +42,17 @@ void		start_game(void *data, const char *filename)
   data_pkt_t	*ptr;
   sfMusic	*music;
   sfSoundBuffer	*buffer;
-  sfSound	*sound;
 
   music = sfMusic_createFromFile(filename);
   buffer = sfSoundBuffer_createFromFile(filename);
-  sound = sfSound_create();
-  if (music == NULL || buffer == NULL || sound == NULL)
+  if (music == NULL || buffer == NULL)
   {
     mdprintf(2, "[%sError%s] Could not open %s%s%s\n",
 	     RED, RESET, YELLOW, filename, RESET);
     return;
   }
-  sfSound_setBuffer(sound, buffer);
   ptr = (data_pkt_t *)data;
-  ingame(ptr->window, ptr->sprites, music, sound);
+  ingame(ptr->window, ptr->sprites, music, buffer);
   sfMusic_destroy(music);
   sfSoundBuffer_destroy(buffer);
-  sfSound_destroy(sound);
 }
