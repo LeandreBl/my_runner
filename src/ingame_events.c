3e66d780c0d3421e45d4c2b2627cef7eedb50e46
Leandre Blanchard <leandre.blanchard@netatmo.com>
2017-12-27 15:19:31 +0100

    events ingame

---
+++ src/ingame_events.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2017
+** runner
+** File description:
+** ingame events handlers
+*/
+
+#include "runner.h"
+
+int		play_button(window_t *window, sfEvent *event, void *data)
+{
+  data_pkt_t	*pkt;
+  sfVector2i	pos;
+
+  (void) window;
+  pkt = (data_pkt_t *)data;
+  pos.x = event->mouseButton.x;
+  pos.y = event->mouseButton.y;
+  sfbutton_exec(*(pkt->buttons), pos, data);
+  return (0);
+}
+
+int		leave_ingame(window_t *window, sfEvent *event, void *data)
+{
+  data_pkt_t	*ptr;
+
+  (void) window;
+  ptr = (data_pkt_t *)data;
+  if (event != NULL && event->type == sfEvtKeyPressed && event->key.code != sfKeyEscape)
+    return (0);
+  //TODO pause / quit screen
+  ptr->back = true;
+  return (0);
+}
